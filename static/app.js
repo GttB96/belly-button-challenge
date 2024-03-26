2a44f9fd254d655261d4b45fc2e8c29fe2227bec
GttB96 <147666979+GttB96@users.noreply.github.com>
2024-03-26 14:41:22 -0400

    Update app.js

--- static/app.js
+++ static/app.js
@@ -1,14 +1,14 @@
-// store source URL
+// source URL
 const url = 'https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json';
 
-// fetch the JSON data and log it
+// JSON data and log it
 d3.json(url).then(function(data){
     console.log(data);
 }); 
 
-//create init function that will populate the dropdown, bar chart, and bubble chart with each sample's dataset
+//create init function to populate the dropdown, bar chart, bubble chart
 function init(){
-    //create the dropdown list variable for all sample id's in the dataset by appending each ID as a new value
+    //create the dropdown list variable for all sample id's 
     let dropdown = d3.select("#selDataset");
     //access sample data using d3
     d3.json(url).then((data) => {
